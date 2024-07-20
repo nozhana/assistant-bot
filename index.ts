d670e7653dff7444dc12c6e0f07eeeb9c2cbbb62
Nozhan Amiri <nozhana@gmail.com>
2024-07-20 12:43:43 +0330

    t26-webhook
    commit f2bf5cbb4c11c975412c1f0b0dd69158fe47226b
    Author: Nozhan Amiri <nozhana@gmail.com>
    Date:   Fri Jul 19 23:40:47 2024 +0330
    
        webhook

--- index.ts
+++ index.ts
@@ -1,13 +1,33 @@
 import bot from "./app/bot";
 
-bot.launch(async () => {
-  await bot.telegram.setMyCommands([
-    { command: "help", description: "💁 List of commands" },
-    { command: "chat", description: "💬 Talk to an assistant" },
-    { command: "assistants", description: "🤖 Manage assistants" },
-    { command: "settings", description: "⚙️ Settings" },
-  ]);
-});
+bot
+  .launch(
+    {
+      webhook: process.env.WEBHOOK_URL
+        ? {
+            domain: process.env.WEBHOOK_URL,
+            port: Number(process.env.WEBHOOK_PORT) || undefined,
+            secretToken: process.env.WEBHOOK_TOKEN,
+            cb: (req, res) =>
+              console.log(`Request: ${req}\nResponse: ${res}\n\n`),
+          }
+        : undefined,
+    },
+    async () => {
+      await bot.telegram.setMyCommands([
+        { command: "help", description: "💁 List of commands" },
+        { command: "chat", description: "💬 Talk to an assistant" },
+        { command: "assistants", description: "🤖 Manage assistants" },
+        { command: "settings", description: "⚙️ Settings" },
+      ]);
+    }
+  )
+  .then(() => {
+    if (process.env.WEBHOOK_URL)
+      console.log(
+        `Webhook listening on ${process.env.WEBHOOK_URL}:${process.env.WEBHOOK_PORT}`
+      );
+  });
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
